d34fb7f13eb222f0f57f54a96f1b05960f9cac1f
Lucas <lteixeira507@gmail.com>
2025-07-31 21:18:47 -0400

    refactor: Improved movie description screen

--- app/src/main/java/dev/lucasteixeira/desafioexploradordefilmes/ui/details/DetailScreen.kt
+++ app/src/main/java/dev/lucasteixeira/desafioexploradordefilmes/ui/details/DetailScreen.kt
@@ -1,5 +1,6 @@
 package dev.lucasteixeira.desafioexploradordefilmes.ui.details
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,16 +10,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
@@ -29,8 +35,11 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import dev.lucasteixeira.desafioexploradordefilmes.data.model.Movie
 import dev.lucasteixeira.desafioexploradordefilmes.ui.main.MoviesViewModel
 import dev.lucasteixeira.desafioexploradordefilmes.ui.main.UiState
 import dev.lucasteixeira.desafioexploradordefilmes.utils.formatDateString
@@ -68,43 +77,14 @@ fun DetailScreen(viewModel: MoviesViewModel, movieId: Int, onNavigateUp: () -> U
         }
     ) { innerPadding ->
         Box(modifier = Modifier.padding(innerPadding)) {
-           when(val state = moviesDetail) {
-               is UiState.Loading -> {
-                   CircularProgressIndicator()
-               }
+            when (val state = moviesDetail) {
+                is UiState.Loading -> {
+                    CircularProgressIndicator()
+                }
 
-               is UiState.Success -> {
-                   val movie = state.data
-                   Column(
-                       modifier = Modifier
-                           .fillMaxSize()
-                           .verticalScroll(rememberScrollState())
-                           .padding(16.dp)
-                   ) {
-                       AsyncImage(
-                           model = "https://image.tmdb.org/t/p/w500${movie.posterPath}",
-                           contentDescription = "Pôster do filme ${movie.title}",
-                           modifier = Modifier
-                               .fillMaxWidth()
-                               .height(400.dp),
-                           contentScale = ContentScale.Fit
-                       )
-                       Spacer(modifier = Modifier.height(16.dp))
-
-                       Text(text = movie.title, style = MaterialTheme.typography.headlineLarge)
-                       Spacer(modifier = Modifier.height(8.dp))
-
-                       Text(
-                           text = "Lançamento: ${formatDateString(movie.releaseDate)}",
-                           style = MaterialTheme.typography.titleLarge
-                       )
-                       Spacer(modifier = Modifier.height(8.dp))
-
-                       Text(text = "Descrição: ", style = MaterialTheme.typography.titleLarge)
-                       Spacer(modifier = Modifier.height(8.dp))
-                       Text(text = movie.overview, style = MaterialTheme.typography.bodyLarge)
-                   }
-               }
+                is UiState.Success -> {
+                    MovieDetailsContent(movie = state.data)
+                }
 
                is UiState.Error -> {
                    Column(
@@ -129,6 +109,93 @@ fun DetailScreen(viewModel: MoviesViewModel, movieId: Int, onNavigateUp: () -> U
            }
         }
     }
+}
+
+@Composable
+fun MovieDetailsContent(movie: Movie) {
+    Column(
+        modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState()).padding(16.dp)
+    ) {
+        Card(
+            modifier = Modifier.fillMaxWidth().height(500.dp),
+            shape = RoundedCornerShape(12.dp),
+            elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+        ) {
+            AsyncImage(
+                model = "https://image.tmdb.org/t/p/w780${movie.posterPath}",
+                contentDescription = "Pôster do filme ${movie.title}",
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop
+            )
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Text(
+            text = movie.title,
+            style = MaterialTheme.typography.headlineLarge,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colorScheme.primary
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
 
 
+        Surface(
+            modifier = Modifier.fillMaxWidth()
+                .border(
+                    width = 1.dp,
+                    color = MaterialTheme.colorScheme.primary.copy(alpha = 0.4f),
+                    shape = RoundedCornerShape(8.dp)
+            ) ,
+            shape = RoundedCornerShape(8.dp),
+            color = MaterialTheme.colorScheme.surface,
+            tonalElevation = 4.dp
+        ) {
+            Column(
+                modifier = Modifier.padding(12.dp)
+            ) {
+                Text(
+                    text = "Lançamento",
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.SemiBold
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = formatDateString(movie.releaseDate),
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Surface(
+            modifier = Modifier.fillMaxWidth()
+                .border(
+                width = 1.dp,
+                color = MaterialTheme.colorScheme.primary.copy(alpha = 0.4f),
+                shape = RoundedCornerShape(8.dp)
+            ),
+            shape = RoundedCornerShape(8.dp),
+            color = MaterialTheme.colorScheme.surface,
+            tonalElevation = 4.dp
+        ) {
+            Column(modifier = Modifier.padding(12.dp)) {
+                Text(
+                    text = "Descrição",
+                    style = MaterialTheme.typography.titleMedium,
+                    fontWeight = FontWeight.SemiBold
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                HorizontalDivider(thickness = 1.dp, color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.1f))
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = movie.overview,
+                    style = MaterialTheme.typography.bodyLarge,
+                    lineHeight = 24.sp
+                )
+            }
+        }
+    }
 }
